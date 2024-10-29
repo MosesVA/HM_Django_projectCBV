48dae52975d867efcc8435d2737cbef339e5d24e
Vladislav Moiseev <morninyep@gmail.com>
2024-10-30 00:14:09 +0500

    fbv user_update_form переписано на cbv UserUpdateForm, внесены необходимые изменения в урлы

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 
 from users.models import User
-from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm, UserPasswordChangeForm
+from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm, UserPasswordChangeForm, UserForm
 from users.services import send_register_mail, send_new_password
 
 
@@ -27,31 +27,23 @@ class UserLoginView(LoginView):
     template_name = 'users/login_user.html'
 
 
-@login_required
-def user_profile_view(request):
-    user_object = request.user
-    context = {
-        'title': f'Ваш профиль {user_object.first_name}',
-    }
-    return render(request, 'users/user_profile_read_only.html', context)
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = 'users/user_profile_read_only.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
 
 
-@login_required
-def user_update_view(request):
-    user_object = request.user
-    if request.method == 'POST':
-        form = UserUpdateForm(request.POST, request.FILES, instance=user_object)
-        if form.is_valid():
-            user_object = form.save()
-            user_object.save()
-            return HttpResponseRedirect(reverse('users:profile_user'))
-    user_name = user_object.first_name
-    context = {
-        'object': user_object,
-        'title': f'Изменить профиль {user_name}',
-        'form': UserUpdateForm(instance=user_object)
-    }
-    return render(request, 'users/update_user.html', context)
+class UserUpdateView(UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    template_name = 'users/update_user.html'
+    success_url = reverse_lazy('users:profile_user')
+
+    def get_object(self, queryset=None):
+        return self.request.user
 
 
 @login_required
