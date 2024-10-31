289089ae5297dfc31c16407f11062bb044e8116e
Vladislav Moiseev <morninyep@gmail.com>
2024-10-31 21:46:31 +0500

    Добавлена модель Parent, выполнена миграция.

--- dogs/models.py
+++ dogs/models.py
@@ -32,5 +32,26 @@ class Dog(models.Model):
         return f'{self.name} ({self.category})'
 
     class Meta:
-        verbose_name = 'dog'
-        verbose_name_plural = 'dogs'
+        verbose_name = 'dog'  # понятное человеку имя модели
+        verbose_name_plural = 'dogs'  # понятное человеку имя модели(множественное число)
+        # abstract = True  # данная модель станет абстрактным базовым классом
+        # app_label = 'dogs'  # если модель определена за пределами app., то можно таким образом ее к нему отнести
+        # ordering = [-1]  # изменение порядка полей в модели
+        # proxy = True  # модель будет рассматриваться как прокси модель
+        # permissions = []  # добавляются группы пользователей, которые могут изменять сущность данной модели
+        # db_table = 'doggies'  # перезаписать имя таблицы в БД
+        # get_latest_by = 'birth_date'  # возвращает последний объект по порядку возрастания (самая молодая собака)
+
+
+class Parent(models.Model):
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250, verbose_name='dog_name')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='breed')
+    birth_date = models.DateField(**NULLABLE, verbose_name='birth_date')
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+    class Meta:
+        verbose_name = 'parent'
+        verbose_name_plural = 'parents'
